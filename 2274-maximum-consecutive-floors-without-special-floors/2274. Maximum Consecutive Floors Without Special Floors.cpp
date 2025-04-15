bbe13be5fcc75dea833283e6547f6124f7a229c1
Kevin Mao <94155782+kev-mao@users.noreply.github.com>
2025-04-14 21:31:18 -0400

    Runtime: 31 ms (51.55%), Memory: 61.9 MB (20.19%) - Gitcode

---
+++ 2274-maximum-consecutive-floors-without-special-floors/2274. Maximum Consecutive Floors Without Special Floors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxConsecutive(int bottom, int top, vector<int>& special) {
+        sort(special.begin(), special.end());
+        int cur = bottom;
+        int ans = special[0] - bottom;
+        for(int i = 1; i < special.size(); i++){
+            ans = max(ans, special[i] - special[i-1] - 1);
+        }
+        ans = max(ans, top - special[special.size()-1]);
+        return ans;
+    }
+};
